4086e42e32233a2c63311bdf46db4b5cdffba49e
Miguel Ângelo <miguelangelo_fm@hotmail.com>
2018-11-26 20:22:37 +0000

    cmt

--- ZombieBird/core/src/com/mygdx/zombiebird/ZombieBird.java
+++ ZombieBird/core/src/com/mygdx/zombiebird/ZombieBird.java
@@ -1,17 +1,21 @@
 package com.mygdx.zombiebird;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.screens.GameScreen;
 
-public class ZombieBird extends ApplicationAdapter {
+public class ZombieBird extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
+		Gdx.app.log("ZombieBird Game", "created");
+		setScreen(new GameScreen());
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
