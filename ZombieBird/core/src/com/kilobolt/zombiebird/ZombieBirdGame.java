c28aa84a2002e941b3e6cd5b584dcd825512dcb2
Miguel Ângelo <miguelangelo_fm@hotmail.com>
2018-11-27 20:43:04 +0000

    cmt

--- ZombieBird/core/src/com/kilobolt/zombiebird/ZombieBirdGame.java
+++ ZombieBird/core/src/com/kilobolt/zombiebird/ZombieBirdGame.java
@@ -6,31 +6,20 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.kilobolt.screens.GameScreen;
+import com.kilobolt.zbhelpers.AssetLoader;
 
 public class ZombieBirdGame extends Game {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		Gdx.app.log("ZombieBirdGame Game", "created");
-		setScreen(new GameScreen());
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        Gdx.app.log("ZBGame", "created");
+        AssetLoader.load();
+        setScreen(new GameScreen());
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        AssetLoader.dispose();
+    }
 }
